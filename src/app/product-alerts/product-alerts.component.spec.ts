5346e315de2385c1fc6228c38ccc4926768cea9c
chengshuo <chengshuo@csii.com.cn>
2022-06-14 11:21:41 +0800

    update angular to 11

--- src/app/product-alerts/product-alerts.component.spec.ts
+++ src/app/product-alerts/product-alerts.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ProductAlertsComponent } from './product-alerts.component';
 
@@ -6,7 +6,7 @@ describe('ProductAlertsComponent', () => {
   let component: ProductAlertsComponent;
   let fixture: ComponentFixture<ProductAlertsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ProductAlertsComponent ]
     })
